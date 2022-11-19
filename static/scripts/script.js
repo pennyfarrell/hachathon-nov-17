831af4771b0047a84fe44d4a14fbad376657bf48
ChrisT-CC <cristaecc@gmail.com>
2022-11-19 17:48:23 +0000

    Add a sound to each key

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,2 +1,14 @@
-// Test link to index.html
-document.body.style.backgroundColor = "red";
+// get all keys
+const keys = document.getElementById("set").children;
+
+// Add sound to each key
+for (let i=0; i<keys.length; i++) {
+    console.log(keys[i]);
+    keys[i].addEventListener('click', () => addSound())
+}
+
+function addSound() {
+    let source = "media/sounds/A.mp3"
+    console.log(source)  // for testing
+    new Audio(source).play()
+}
