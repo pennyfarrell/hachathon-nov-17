ea537aca61c97e06ae5e3194b3cd03907afe9e5d
ChrisT-CC <cristaecc@gmail.com>
2022-11-19 20:21:27 +0000

    Added different sounds to each key: - Add Id to each key - Style the keys accordingly to new id's - Add soundSource function to generate different sound url's

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,12 +3,56 @@ const keys = document.getElementById("set").children;
 
 // Add sound to each key
 for (let i=0; i<keys.length; i++) {
-    console.log(keys[i]);
-    keys[i].addEventListener('click', () => addSound())
+    // console.log(keys[i]);  // for testing
+    let keyId = keys[i].getAttribute("id");
+    console.log(keyId);
+    keys[i].addEventListener('click', () => addSound(keyId))
 }
 
-function addSound() {
-    let source = "media/sounds/A.mp3"
+function addSound(s) {
+    let source = soundSource(s);
     console.log(source)  // for testing
     new Audio(source).play()
+}
+
+function soundSource(src) {
+    let path = "media/sounds/";
+    switch (src){
+        case "a":
+            return path += "a.mp3";
+            break;
+        case "as":
+            return path += "as.mp3";
+            break;
+        case "b":
+            return path += "b.mp3";
+            break;
+        case "c":
+            return path += "c.mp3";
+            break;
+        case "cs":
+            return path += "cs.mp3";
+            break;
+        case "d":
+            return path += "d.mp3";
+            break;
+        case "ds":
+            return path += "ds.mp3";
+            break;
+        case "e":
+            return path += "e.mp3";
+            break;
+        case "f":
+            return path += "f.mp3";
+            break;
+        case "fs":
+            return path += "fs.mp3";
+            break;
+        case "g":
+            return path += "g.mp3";
+            break;
+        case "gs":
+            return path += "gs.mp3";
+            break;
+    }
 }
